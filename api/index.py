d50db44f7f4dcd18c8d171adac34ea989ab8e525
Jan William Haug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-01-28 15:45:29 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-"""
-from flask import Flask
+
+#from flask import Flask
+from flask import Flask, jsonify
+import gdown
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    #return 'Hello, World!'
+    gdown.download('https://drive.google.com/drive/folders/1UG3LP3mwzoCUcsWmXQ2TMPOp91tJ_dnt', 'colab.ipynb', quiet=False)
+    return jsonify(message='colab notebook ran successfully')
 
 @app.route('/about')
 def about():
@@ -21,3 +25,4 @@ def hello_world():
     #return "Hello, World!"
     gdown.download('https://drive.google.com/drive/folders/1UG3LP3mwzoCUcsWmXQ2TMPOp91tJ_dnt', 'colab.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
+"""
