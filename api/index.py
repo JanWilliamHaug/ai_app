c530cb54dd1443c18038581fc926b9aea99145ac
Jan William Haug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-01-28 23:47:11 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,8 +7,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
+
+if "__name__" == "__main__":
+    app.run(debug=True)
+    
+#def home():
+    #return 'Hello, World!'
     #gdown.download('https://drive.google.com/drive/folders/1UG3LP3mwzoCUcsWmXQ2TMPOp91tJ_dnt', 'colab.ipynb', quiet=False)
     #return jsonify(message='colab notebook ran successfully')
 
